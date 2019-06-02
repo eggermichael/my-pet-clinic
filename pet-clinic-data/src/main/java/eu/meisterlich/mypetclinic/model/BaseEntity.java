c15f8c6a0dfdcf2b7cd91a8abe61aad2bfdb972a
Michael Egger <michael.egger@gmx.biz>
2019-06-02 09:09:35 +0200

    add base entity with id. closes #10

---
+++ pet-clinic-data/src/main/java/eu/meisterlich/mypetclinic/model/BaseEntity.java
@@ -0,0 +1,14 @@
+package eu.meisterlich.mypetclinic.model;
+
+public class BaseEntity {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
--- pet-clinic-data/src/main/java/eu/meisterlich/mypetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/eu/meisterlich/mypetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package eu.meisterlich.mypetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String petType;
 
