15b145a5c0e1716fe1044ee65998c903310861af
Michael Egger <michael.egger@gmx.biz>
2019-06-12 20:48:19 +0200

    list vets and owners on index pages. closes #15. closes #16

--- pet-clinic-web/src/main/java/eu/meisterlich/mypetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/eu/meisterlich/mypetclinic/controllers/OwnerController.java
@@ -1,15 +1,25 @@
 package eu.meisterlich.mypetclinic.controllers;
 
 
+import eu.meisterlich.mypetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
-    @RequestMapping({"/owners", "owners", "/owners.html"})
-    public String getOwners() {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/", "/owners", "owners", "/owners.html"})
+    public String getOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners";
     }
 }
--- pet-clinic-web/src/main/java/eu/meisterlich/mypetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/eu/meisterlich/mypetclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package eu.meisterlich.mypetclinic.controllers;
 
+import eu.meisterlich.mypetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "vets", "/vets.html"})
-    public String getVets() {
+    public String getVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets";
     }
 
